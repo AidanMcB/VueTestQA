f4b0f7c6de1131d3b4368b246736787bf2bcd7a1
Aidan McBride <aidankmcbride@gmail.com>
2020-09-11 12:44:57 -0400

    fixes radio button error

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -2,16 +2,10 @@
   <div class="question-card">
     {{ question.id}}.
     {{ question.question}}
-    <!-- :key="`${ key }-${ index }`" -->
-    <!-- v-bind:key="index" -->
-    <!-- <div class="optionals" v-for="(key, index) in question.options" v-bind:key="index"> -->
-    <!-- <input type="radio" :value="index"/> -->
-    <!-- {{ key }} -->
-    <!-- @change="selectWinner(game.id, game.pollanswers[indexy].id)" -->
-    <div v-for="(answers, index) in question.options" v-bind:key="answers.id">
-      <input type="radio" :value="question.options[index].id" v-model="question.id" />
-      <span>{{answers.id}} || {{index}} || {{key }}</span>
-      <label for="one">{{question.options[index].answer}}</label>
+    <div v-for="(answers, index, key) in question.options" v-bind:key="index">
+      <input type="radio" :value="question.options[index].id" v-model="question.id"/>
+      <span > {{ key }}</span>
+      <label >{{question.options[index].answer}}</label>
       <br />
     </div>
   </div>
