e7b0c0c436bdf4c89b18fb0d046011eeefab32ab
Aidan McBride <aidankmcbride@gmail.com>
2020-09-11 00:56:14 -0400

    fixes same choice key error, still has binding key error

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -5,20 +5,16 @@
     <!-- :key="`${ key }-${ index }`" -->
     <!-- v-bind:key="index" -->
     <!-- <div class="optionals" v-for="(key, index) in question.options" v-bind:key="index"> -->
-      <!-- <input type="radio" :value="index"/> -->
-      <!-- {{ key }} -->
-          <!-- @change="selectWinner(game.id, game.pollanswers[indexy].id)" -->
-      <div v-for="(answers, index, value) in question.options" :key="answers.id + index">
-       <input
-          type="radio"
-          :value="question.options[index].id"
-          v-model="question.id"
-        />
-        <span> {{answers.id }}|| {{index}} || {{value}} </span>
-        <label for="one">{{question.options[index].answer}}</label>
-        <br />
-        </div>
+    <!-- <input type="radio" :value="index"/> -->
+    <!-- {{ key }} -->
+    <!-- @change="selectWinner(game.id, game.pollanswers[indexy].id)" -->
+    <div v-for="(answers, index) in question.options" v-bind:key="answers.id">
+      <input type="radio" :value="question.options[index].id" v-model="question.id" />
+      <span>{{answers.id}} || {{index}} || {{key }}</span>
+      <label for="one">{{question.options[index].answer}}</label>
+      <br />
     </div>
+  </div>
 </template>
 
 <script>
