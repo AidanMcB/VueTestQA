a4e823f5f8458b8e7c31e3764de3d424195e225b
Aidan McBride <aidankmcbride@gmail.com>
2020-09-11 00:25:05 -0400

    input radio boxes

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -2,10 +2,23 @@
   <div class="question-card">
     {{ question.id}}.
     {{ question.question}}
-    <ul class="options">
-        <li v-bind:key="option.index" v-for="option in options">{{option}}</li>
-    </ul>
-  </div>
+    <!-- :key="`${ key }-${ index }`" -->
+    <!-- v-bind:key="index" -->
+    <!-- <div class="optionals" v-for="(key, index) in question.options" v-bind:key="index"> -->
+      <!-- <input type="radio" :value="index"/> -->
+      <!-- {{ key }} -->
+          <!-- @change="selectWinner(game.id, game.pollanswers[indexy].id)" -->
+      <div v-for="(answers, index, value) in question.options" :key="answers.id + index">
+       <input
+          type="radio"
+          :value="question.options[index].id"
+          v-model="question.id"
+        />
+        <span> {{answers.id }}|| {{index}} || {{value}} </span>
+        <label for="one">{{question.options[index].answer}}</label>
+        <br />
+        </div>
+    </div>
 </template>
 
 <script>
